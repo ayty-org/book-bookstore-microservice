2fccd4c52b56cbb81beef3e714b6fce8ebc08485
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-02-24 22:07:41 -0300

    feat(#4): add anotation to config project with eureka client

--- src/main/java/br/com/bookstore/book/BookApplication.java
+++ src/main/java/br/com/bookstore/book/BookApplication.java
@@ -2,8 +2,12 @@ package br.com.bookstore.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class BookApplication {
 
 	public static void main(String[] args) {
