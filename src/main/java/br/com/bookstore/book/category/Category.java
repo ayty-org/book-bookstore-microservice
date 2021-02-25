e7d1adfa748b3409a4b3cc37c32715f21c84c48e
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-02-24 22:37:02 -0300

    feat(#6): create Category

---
+++ src/main/java/br/com/bookstore/book/category/Category.java
@@ -0,0 +1,29 @@
+package br.com.bookstore.book.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder(builderClassName = "Builder")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
