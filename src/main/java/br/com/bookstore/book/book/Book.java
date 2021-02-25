f7d99c72be941c19528ee50bb51266c0ec2f4546
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-02-24 21:44:21 -0300

    feat(#1): create project and entity Book

---
+++ src/main/java/br/com/bookstore/book/book/Book.java
@@ -0,0 +1,47 @@
+package br.com.bookstore.book.book;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder(builderClassName = "Builder")
+public class Book implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String sinopse;
+
+    private String autor;
+
+    private String isbn;
+
+    private LocalDate yearOfPublication;
+
+    private double sellPrice;
+
+    private int quantityAvailable;
+
+    private UUID specificID;
+
+    private UUID purchaseID;
+}
