aef8819d807cdbcd5623358b0c059644030bd749
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-02-24 21:46:54 -0300

    feat(#2): create BookRepository

---
+++ src/main/java/br/com/bookstore/book/book/BookRepository.java
@@ -0,0 +1,8 @@
+package br.com.bookstore.book.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
