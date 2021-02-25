61907d0cff5589c1f14eee410db3cd1887c4dff1
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-02-24 22:59:18 -0300

    feat(#8): create DeleteCategoryService and CategoryNotFound

---
+++ src/main/java/br/com/bookstore/book/category/services/DeleteCategoryService.java
@@ -0,0 +1,6 @@
+package br.com.bookstore.book.category.services;
+
+@FunctionalInterface
+public interface DeleteCategoryService {
+    void delete(Long id);
+}
---
+++ src/main/java/br/com/bookstore/book/category/services/DeleteCategoryServiceImpl.java
@@ -0,0 +1,23 @@
+package br.com.bookstore.book.category.services;
+
+import br.com.bookstore.book.category.CategoryRepository;
+import br.com.bookstore.book.category.services.DeleteCategoryService;
+import br.com.bookstore.book.exceptions.CategoryNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteCategoryServiceImpl implements DeleteCategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public void delete(Long id) {
+        if (!categoryRepository.existsById(id)) {
+            throw new CategoryNotFoundException();
+        }
+
+        categoryRepository.deleteById(id);
+    }
+}
